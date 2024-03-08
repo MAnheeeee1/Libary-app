08c7b17624fad5e1abb0e06bb8d04e27de2272d0
Manh <07mandao@skola.boras.se>
2024-03-08 19:47:16 +0100

    s

--- script.js
+++ script.js
@@ -1,22 +1,17 @@
 book_libary = document.querySelector(".bookLibary");
-testbook = {
-    title : "Think And Grow Rich",
-    writer : "Napoleon Hill",
-    pages : "269",
-    readStatus : "read"
-}
 
 const button_form = document.querySelector("button");
 const dialog = document.querySelector("dialog");
-const close_button = document.querySelector("#close-dialog")
-const submitButton = document.querySelector("#submit-form")
+const close_button = document.querySelector("#close-dialog");
+const submitButton = document.querySelector("#submit-form");
 const form = document.querySelector("form");
+
 submitButton.addEventListener("click", ()=>{
     book_writer = document.querySelector("#book-writer");
     book_title = document.querySelector("#book_title");
     book_pages = document.querySelector("#book_pages");
     book_status = document.querySelector("#read-status");
-    let book = new Book(book_title.value, book_writer.value, book_pages.value, book_status.value);
+    let book = new Book(book_title.value, book_writer.value, book_pages.value, book_status.value, myLibrary.length);
     addBookToLibrary(book);
     updateLibary();
     const emptyLibaryText = document.querySelector("#emptylibary-text");
@@ -27,15 +22,16 @@ button_form.addEventListener("click", () =>{
     dialog.showModal();
 })
 close_button.addEventListener("click", () =>{
-    dialog.close()
+    dialog.close();
 })
 
-const myLibrary= [testbook];
-function Book(title, writer, pages, readStatus) {
+const myLibrary= [];
+function Book(title, writer, pages, readStatus, bookIndex) {
     this.title = title;
     this.writer = writer;
     this.pages = pages;
     this.readStatus = readStatus;
+    this.bookIndex = bookIndex;
 }
 
 function addBookToLibrary(book) {
@@ -45,7 +41,9 @@ function addBookToLibrary(book) {
 function updateLibary(){
     let addedBook = myLibrary[myLibrary.length -1];
     let book_card = document.createElement("div")
-    book_card.classList.add("card")
+    book_card.classList.add("card");
+    let bookIndex = addedBook.bookIndex
+    book_card.classList.add(bookIndex);
 
     let book_title = document.createElement("h2");
     book_title.innerHTML = addedBook.title;
@@ -59,9 +57,21 @@ function updateLibary(){
     let book_readStatus = document.createElement("p");
     book_readStatus.innerHTML = addedBook.status;
 
+    let deleteButton = document.createElement("button");
+    deleteButton.classList.add("deleteBook");
+    deleteButton.innerHTML = "Delete Book";
+    deleteButton.addEventListener("click", ()=>{
+        book_card.remove();
+    })
     book_libary.appendChild(book_card);
     book_card.appendChild(book_title);
     book_card.appendChild(book_writer);
     book_card.appendChild(book_pages);
     book_card.appendChild(book_readStatus);
+    book_card.appendChild(deleteButton);
 }
+
+
+
+
+
